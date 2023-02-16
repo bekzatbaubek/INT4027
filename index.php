f8539991a5f485b45c535f2736be3357dc3a41da
Bekzat Baubek <39556077+bekzatbaubek@users.noreply.github.com>
2023-02-16 05:33:58 +0000

    basic skeleton

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+            margin: auto;
+            max-width: 800px;
+            padding: 12px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Test Website</h1>
+    <?php
+    echo " Today is " . date("l") . ". ";
+    ?>
+    <p>another paragraph</p>
+</body>
+
+</html>
